8983bd23386f6303eb172aef1b9c3771c0d40ca1
bitcubix <bitcubix@bitcubix.io>
2021-12-24 16:28:34 +0100

    rewrite cmd class with Popen and logging

--- lib/cmd.py
+++ lib/cmd.py
@@ -1,46 +1,67 @@
 """shell interaction"""
 import subprocess
-from user import User
+from . import Logger, User
+
+log = Logger.get('cmd')
 
 
 class Cmd:
     """shell interaction"""
 
     @staticmethod
-    def exec(cmd, exec_user='', log_cmd=True, io_stream_type=0):
+    def exec(cmd, exec_user='', log_cmd=True, io_stream_type=0,
+             ignore_error=False):
         """
         run shell commands with optional io stream
-        io_stream_type: 0=none, 1=stdout, 2=log
+        io_stream_type: 0=stdout, 1=internal, 2=log, 3=suppressed
         """
         if exec_user == '' or not User().user_exists(exec_user):
             exec_user = User.get_current_user()
 
-        exec_cmd = f"sudo -i -u {exec_user} -H bash -c '{cmd}'"
+        exec_cmd = ["sudo", "-i", "-u", exec_user,
+                    "-H", "bash", "-c", cmd]
 
         if log_cmd:
-            #TODO use logger to log cmd
+            log.info(f"exec: {cmd}", extra={'user': exec_user})
+        else:
+            log.debug(f"exec: {cmd}", extra={'user': exec_user})
 
         if io_stream_type == 0:
-            exec_cmd = exec_cmd + ' &> /dev/null'
-            res = subprocess.run(exec_cmd, shell=True, check=True)
-        if io_stream_type == 1:
-            res = subprocess.run(exec_cmd, shell=True,
-                                 encoding='utf-8', capture_output=True, check=True)
-            if res.returncode != 0:
-                print(res.stderr)
-            else:
-                print(res.stdout)
-        if io_stream_type == 2:
-            res = subprocess.run(exec_cmd, shell=True, check=True)
-            if res.returncode != 0:
-                print('log stderr')
-            else:
-                print('log stdout')
-
-        if res.returncode == 0:
-            return True
-
-        return False
+            with subprocess.Popen(exec_cmd) as proc:
+                if proc.returncode == 0:
+                    return True
+                return False
+        else:
+            with subprocess.Popen(exec_cmd,
+                                  stderr=subprocess.PIPE,
+                                  stdout=subprocess.PIPE) as proc:
+                res_code = proc.returncode
+                if io_stream_type == 1:
+                    if res_code == 0:
+                        return res_code, proc.stdout.read().decode('utf-8')
+                    return res_code, proc.stderr.read().decode('utf-8')
+                elif io_stream_type == 2:
+                    if res_code == 0:
+                        log_msg = proc.stdout.read().decode(
+                            'utf-8').replace('\n', '')
+                        log.info(f"out: {log_msg}",
+                                 extra={'cmd': cmd, 'user': exec_user})
+                    else:
+                        log_err = proc.stderr.read().decode(
+                            'utf-8').replace('\n', '')
+                        extra = {'cmd': cmd, 'user': exec_user}
+                        if res_code is not None:
+                            extra['code'] = res_code
+                        if ignore_error:
+                            log.debug(f"{log_err}",
+                                      extra={'cmd': cmd,
+                                             'code': res_code,
+                                             'user': exec_user})
+                        else:
+                            log.warning(f"{log_err}",
+                                        extra={'cmd': cmd,
+                                               'code': res_code,
+                                               'user': exec_user})
 
     @staticmethod
     def exec_suppress(cmd, exec_user=''):
